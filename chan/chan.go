0d9dbc33cd38d8e7788d69e376a63703ef822efe
Mo <dabiggmoe@gmail.com>
2019-06-11 21:16:08 +0300

    fixed "goroutines sleep deadlock" in chanchan/chanchan.go. Working now

---
+++ chan/chan.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ch := make(chan int)
+	go func() {
+		ch <- 1
+		ch <- 2
+		ch <- 3
+		close(ch)
+	}()
+	for n := range ch {
+		fmt.Println(n)
+	}
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
--- chanchan/chanchan.go
+++ chanchan/chanchan.go
@@ -13,40 +13,47 @@ type Request struct {
 
 func sum(nums []int) int {
 	var sum int
-	for n := range nums {
+	for _, n := range nums {
 		sum += n
 	}
 	return sum
 }
 
-func handle(queue chan *Request, wg *sync.WaitGroup) {
-	fmt.Println("Inside handle")
+func handle(queue chan *Request, results chan *Request, wg *sync.WaitGroup) {
+	fmt.Println("[handle] Inside handle")
 
 	for r := range queue {
+		fmt.Println("[handle] Reading queue")
 		r.resultChan <- r.f(r.args)
 		close(r.resultChan)
-		fmt.Println("Done goroutine")
+		fmt.Println("[handle] Closing queue")
+		results <- r
 		wg.Done()
+		fmt.Println("[handle] Done goroutine")
 	}
 
+	close(results)
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
-	request := &Request{[]int{1, 2, 3, 4}, sum, make(chan int)}
+	request := &Request{[]int{1, 2, 3, 4}, sum, make(chan int, 2)}
 	queue := make(chan *Request, 100)
+	results := make(chan *Request, 100)
+
+	fmt.Printf("[main] Adding %v to the queue\n", *request)
 	wg.Add(1)
 	queue <- request
-
+	fmt.Println("[main] closing the queue")
 	close(queue)
-	handle(queue, &wg)
+	fmt.Println("[main] calling handle")
+	go handle(queue, results, &wg)
 
-	//	wg.Wait()
-	fmt.Printf("len = %v, size = %v", len(queue), cap(queue))
+	wg.Wait()
+	fmt.Printf("len = %v, size = %v\n", len(results), cap(results))
 
-	//fmt.Printf("The sum of %v is %v", r.args, <-r.resultChan)
-	/*for r := range queue {
-		fmt.Printf("The sum of %v is %v", r.args, <-r.resultChan)
-	}*/
+	for r := range results {
+		fmt.Printf("The sum of %v is %v\n", r.args, <-r.resultChan)
+	}
 }
